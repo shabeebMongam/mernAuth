cd559b25b3b3160c4211b3673792ad27e937b982
SHABEEB K <shabeebmongam@gmail.com>
2023-08-19 11:01:55 +0530

    create test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,5 @@
+export const test =()=>{
+    res.json({
+        message:"Hello World"
+    })
+}
--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config()
 import express from 'express'
 import mongoose from 'mongoose';
-
+import userRouter from './routes/user.route.js'
 
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("DB connected");
@@ -16,3 +16,6 @@ const app = express()
 app.listen(3000,()=>{
     console.log("On port 3000");
 })
+
+
+app.use("/api/user",userRouter)
