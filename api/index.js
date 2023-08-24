fb45785f5540b0f1432890d5a161b41f885efc23
Shabeeb <shabeebmongam@gmail.com>
2023-08-24 07:34:30 +0530

    sign up api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ dotenv.config()
 import express from 'express'
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("DB connected");
@@ -11,6 +12,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
 })
 
 const app = express()
+app.use(express.json())
 
 
 app.listen(3000,()=>{
@@ -19,3 +21,4 @@ app.listen(3000,()=>{
 
 
 app.use("/api/user",userRouter)
+app.use("/api/auth",authRouter)
